913990819bfb64b4edd9cbc7d97cec6b90440481
mykhailo <mykhailok96@gmail.com>
2022-04-02 18:58:14 +0300

    commit message

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const linkedin = document.querySelector("#linkedin");
+linkedin.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.open(
+    "https://www.linkedin.com/in/mykhailo-konchuk-4b2820176/",
+    "_blank"
+  );
+});
